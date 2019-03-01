d51a9bb2fee80b614dcfccaee20061fa8a8b6c88
jiwu.wyy <jiwu.wyy@alibaba-inc.com>
2019-03-01 21:19:06 +0800

    chore: update gatsby configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
         icon: `content/assets/gatsby-icon.png`,
       },
     },
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
