1d7a0116f507ac9eacf163a373fea0f5c313f90e
jiwu.wyy <jiwu.wyy@alibaba-inc.com>
2019-03-22 15:06:11 +0800

    feat: old url redirect loaded

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,6 +11,7 @@ class NotFoundPage extends React.Component {
 
     this.state = {
       loaded: false,
+      toRedirect: false,
     }
   }
   componentDidMount() {
@@ -20,26 +21,29 @@ class NotFoundPage extends React.Component {
   redirectBlogPrefix() {
     const { pathname } = window.location;
     const regex = /\/blog\/archives/;
+    let toRedirect = false;
 
     if (regex.test(pathname)) {
       navigate(pathname.replace(regex, ''));
+      toRedirect = true;
     }
 
     this.setState({
       loaded: true,
+      toRedirect,
     });
   }
 
   render() {
     const { data } = this.props
-    const { loaded } = this.state;
+    const { loaded, toRedirect } = this.state;
     const siteTitle = data.site.siteMetadata.title
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
 
-        { loaded ? (
+        { (loaded && !toRedirect) ? (
           <Fragment>
             <h1>Oops! 进入了一片荒芜之地...</h1>
             <p>小伍很懒，还没有想好在404放什么有趣的内容~</p>
