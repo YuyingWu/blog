ac5489ec3016028906fa0088490b03ac5658ffcb
jiwu.wyy <jiwu.wyy@alibaba-inc.com>
2019-03-22 14:44:27 +0800

    feat: old url redirect loaded

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
@@ -6,6 +6,13 @@ import SEO from '../components/seo'
 import { navigate } from 'gatsby'
 
 class NotFoundPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false,
+    }
+  }
   componentDidMount() {
     this.redirectBlogPrefix();
   }
@@ -17,18 +24,28 @@ class NotFoundPage extends React.Component {
     if (regex.test(pathname)) {
       navigate(pathname.replace(regex, ''));
     }
+
+    this.setState({
+      loaded: true,
+    });
   }
 
   render() {
     const { data } = this.props
+    const { loaded } = this.state;
     const siteTitle = data.site.siteMetadata.title
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Oops! 进入了一片荒芜之地...</h1>
-        <p>小伍很懒，还没有想好在404放什么有趣的内容~</p>
-        <p>你可能想 >> <a href="/">回到首页</a></p>
+
+        { loaded ? (
+          <Fragment>
+            <h1>Oops! 进入了一片荒芜之地...</h1>
+            <p>小伍很懒，还没有想好在404放什么有趣的内容~</p>
+            <p>你可能想 >> <a href="/">回到首页</a></p>
+          </Fragment>
+        ) : <p>加载中……</p> }
       </Layout>
     )
   }
