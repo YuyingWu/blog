4389784c0f04901bf52e7f921760be68c821d66c
Yuying Wu <wuyuying1128@163.com>
2019-10-13 22:50:06 +0800

    feat: ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-74424222-4`,
       },
     },
     `gatsby-plugin-feed`,
