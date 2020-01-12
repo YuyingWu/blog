32cfe66d31a8c7988b7a78c43dd567b9a44b1f47
jiwu.wyy <jiwu.wyy@alibaba-inc.com>
2020-01-12 13:58:47 +0800

    feat: wx share

---
+++ src/gatsby-theme-chaton/components/layout.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { css, Styled } from 'theme-ui'
+import Header from 'gatsby-theme-chaton/src/components/header'
+import Helmet from 'react-helmet'
+
+export default ({ children, ...props }) => (
+  <Styled.root>
+    <Helmet>
+      <script src="https://res.wx.qq.com/open/js/jweixin-1.6.0.js" />
+      <script>
+        {`
+        wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
+          wx.updateTimelineShareData({ 
+            title: '我的2019 ｜ Yuying Wu', // 分享标题
+            link: 'https://wuyuying.com/year-2019/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'https://static.wuyuying.com/travel.jpg', // 分享图标
+            success: function () {
+              // 设置成功
+            }
+          })
+        });
+        `}
+      </script>
+    </Helmet>
+    <Header {...props} />
+    <div>
+      <div
+        css={css({
+          maxWidth: `container`,
+          mx: `auto`,
+          px: 2,
+          py: 3,
+        })}
+      >
+        {children}
+      </div>
+    </div>
+  </Styled.root>
+)
